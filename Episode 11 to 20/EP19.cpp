863efac8aa879e78960fbb270455ecf519d01efa
karakib2k18 <karakib2k18@gmail.com>
2022-01-29 00:04:11 +0600

    ep19

---
+++ Episode 11 to 20/EP19.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// //sum of a list-> 1 2 3 4 5 6 7 
+// int sum(int n, int a[]){
+// 	if(n<0) return 0;
+// 	return sum(n-1, a)+ a[n];
+// }
+
+// //sum of long digit -> 12345678
+// int digit_sum(int n){
+// 	if(n<0) return 0;
+// 	return digit_sum(n/10) + n%10;
+// }
+
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+	// //selection sort
+	// int n;
+	// cin>>n;
+	// int a[n];
+	// for(int i=0; i<n; ++i){
+	// 	cin>> a[i];
+	// }
+	// for(int i=0; i<n; ++i){
+	// 	int mindindex = i;
+	// 	for(int j=i+1; j<n; ++j){
+	// 		if(a[j]<a[mindindex]){
+	// 			mindindex = j;
+	// 		}
+	// 	}
+	// 	swap(a[i], a[mindindex]);
+	// }
+	// for(int i=0; i<n; ++i){
+	// 	cout<< a[i] << " ";
+	// }
+}
